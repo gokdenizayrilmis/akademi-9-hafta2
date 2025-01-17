884f17c7a22f5aac66d31ea1c70929f9b498e952
gokdeniz <ayrilmis.gokdeniz@gmail.com>
2025-01-18 01:15:01 +0300

    Pseudo kod yazıldı.

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akademi
+{
+    internal class program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+             // Bir online bankacılık uygulamasından para gönderilirken, öncelikle limit kontrol edilecek. 
+             //* Limit 0'dan büyükse para gönderme işlemi gerçekleştirilebilecek.
+             //* Gönderilen para miktarı 100 türk lirasından az ise 1 tl ücret işlemi alınacak. 
+             //* 100 lira ve 100 lira arasında ise 3 tl ücret işlemi alınacak.
+             //* 1000 liradan fazla ise 5 tl ücret işlemi uygulanacak. 
+             //Limit ve gönderilmek istenen para miktarı girildiğinde toplam tutarı ekrana yazdıran uygulamayı yaz.
+
+
+            // bakiye = Lutfen bakiyenizi giriniz.
+            // islemucreti = 0
+            // if(bakiye <= 0){
+            //
+            // print Limitiniz yetersiz olduğundan işlem gerçekleştirilemiyor.
+            // } 
+            //  
+            //  gonderilecekPara = Kac para gondermek istiyorsunuz?
+            //
+            //  if(gonderilecekPara < 100){
+            //  islemucreti = 1
+            //  gonderilekPara = gonderilekPara + islemucreti
+            //  if(gonderilecekpara > bakiye){
+            //
+            //  print bakiyeniz yetersiz. Tekrar deneyin
+            //  return
+            //  }
+            //      bakiye = bakiye - gonderilecekPara;
+            //
+            //      print islemUcreti + " tl islem ucreti kesildi. " + "Bakiyenizden " + gonderilecekPara  + " tl para kesildi. " + " Guncel Bakiyeniz: " + bakiye
+            // }
+            // 
+            //  elseif(gonderilecekPara > 100 && gonderilecekPara < 1000)
+            //  {
+            //
+            //  islemUcreti = 3
+            //  gonderilekPara = gonderilekPara + islemucreti
+            //  if(gonderilecekpara > bakiye)
+            //    {
+            //
+            //  print bakiyeniz yetersiz. Tekrar deneyin
+            //  return
+            //    }
+            //      bakiye = bakiye - gonderilecekPara;
+            //      print islemUcreti + " tl islem ucreti kesildi. " + "Bakiyenizden " + gonderilecekPara  + " tl para kesildi. " + " Guncel Bakiyeniz: " + bakiye
+            //  }
+            //
+            // elseif(gonderilecekPara > 1000)
+            //  {
+            //
+            //  islemUcreti = 5
+            //  gonderilekPara = gonderilekPara + islemucreti
+            //  if(gonderilecekpara > bakiye)
+            //    {
+            //
+            //  print bakiyeniz yetersiz. Tekrar deneyin
+            //  return
+            //    }
+            //      bakiye = bakiye - gonderilecekPara;
+            //      print islemUcreti + " tl islem ucreti kesildi. " + "Bakiyenizden " + gonderilecekPara  + " tl para kesildi. " + " Guncel Bakiyeniz: " + bakiye
+            //  }
+            //
+            // else{
+            //
+            //
+            //  print Gecersiz miktar girdiniz. Lutfen tekrar deneyiniz.
+            // }
+
+            
+
+
+
+        }
+
+    }
+}

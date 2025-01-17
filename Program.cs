306ea373eef2846b3acb462988e1a60690540382
gokdeniz <ayrilmis.gokdeniz@gmail.com>
2025-01-18 01:16:09 +0300

    Kod yazıldı.

--- Program.cs
+++ Program.cs
@@ -13,12 +13,12 @@ namespace Akademi
         {
 
 
-             // Bir online bankacılık uygulamasından para gönderilirken, öncelikle limit kontrol edilecek. 
-             //* Limit 0'dan büyükse para gönderme işlemi gerçekleştirilebilecek.
-             //* Gönderilen para miktarı 100 türk lirasından az ise 1 tl ücret işlemi alınacak. 
-             //* 100 lira ve 100 lira arasında ise 3 tl ücret işlemi alınacak.
-             //* 1000 liradan fazla ise 5 tl ücret işlemi uygulanacak. 
-             //Limit ve gönderilmek istenen para miktarı girildiğinde toplam tutarı ekrana yazdıran uygulamayı yaz.
+            // Bir online bankacılık uygulamasından para gönderilirken, öncelikle limit kontrol edilecek. 
+            //* Limit 0'dan büyükse para gönderme işlemi gerçekleştirilebilecek.
+            //* Gönderilen para miktarı 100 türk lirasından az ise 1 tl ücret işlemi alınacak. 
+            //* 100 lira ve 100 lira arasında ise 3 tl ücret işlemi alınacak.
+            //* 1000 liradan fazla ise 5 tl ücret işlemi uygulanacak. 
+            //Limit ve gönderilmek istenen para miktarı girildiğinde toplam tutarı ekrana yazdıran uygulamayı yaz.
 
 
             // bakiye = Lutfen bakiyenizi giriniz.
@@ -79,7 +79,88 @@ namespace Akademi
             //  print Gecersiz miktar girdiniz. Lutfen tekrar deneyiniz.
             // }
 
-            
+            Console.Write("Lutfen limitinizi giriniz: ");
+            int bakiye = Convert.ToInt32(Console.ReadLine());
+            int islemUcreti = 0;
+
+            if (bakiye <= 0)
+            {
+                Console.WriteLine("Limitiniz yetersiz. Lutfen bakiyenizi kontrol edin.");
+                return;
+                Console.Read();
+            }
+
+            Console.Write("Gondermek istediginiz ucreti giriniz: ");
+            int gonderilecekPara = Convert.ToInt32(Console.ReadLine());
+
+            if (gonderilecekPara < 100)
+            {
+
+                islemUcreti = 1;
+
+                gonderilecekPara = gonderilecekPara + islemUcreti;
+
+                if (gonderilecekPara > bakiye)
+                {
+
+                    Console.WriteLine("Bakiyeniz yetersiz. Lutfen bakiyenizi kontrol edin.");
+                    return;
+                    Console.Read();
+
+                }
+
+                bakiye = bakiye - gonderilecekPara;
+
+                Console.WriteLine(islemUcreti + " tl islem ucreti kesildi. " + "Bakiyenizden " + gonderilecekPara + " tl para kesildi. " + " Guncel Bakiyeniz: " + bakiye);
+            }
+
+            else if (gonderilecekPara > 100 && gonderilecekPara < 1000)
+            {
+
+                islemUcreti = 3;
+
+                gonderilecekPara = gonderilecekPara + islemUcreti;
+
+                if (gonderilecekPara > bakiye)
+                {
+
+                    Console.WriteLine("Bakiyeniz yetersiz. Lutfen bakiyenizi kontrol edin.");
+                    return;
+                    Console.Read();
+
+                }
+
+                bakiye = bakiye - gonderilecekPara;
+
+                Console.WriteLine(islemUcreti + " tl islem ucreti kesildi. " + "Bakiyenizden " + gonderilecekPara + " tl para kesildi. " + " Guncel Bakiyeniz: " + bakiye);
+            }
+
+            else if (gonderilecekPara >= 1000)
+            {
+
+                islemUcreti = 5;
+
+                gonderilecekPara = gonderilecekPara + islemUcreti;
+
+                if (gonderilecekPara > bakiye)
+                {
+
+                    Console.WriteLine("Bakiyeniz yetersiz. Lutfen bakiyenizi kontrol edin.");
+                    return;
+                    Console.Read();
+
+                }
+
+                bakiye = bakiye - gonderilecekPara;
+
+                Console.WriteLine(islemUcreti + " tl islem ucreti kesildi. " + "Bakiyenizden " + gonderilecekPara + " tl para kesildi. " + " Guncel Bakiyeniz: " + bakiye);
+            }
+
+            else
+            {
+                Console.WriteLine("Gecersiz miktar girdiniz. Lutfen tekrar deneyiniz.");
+                return;
+            }
 
 
 
